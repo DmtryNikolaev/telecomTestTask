d9ab343cca56bd866aad68f59a6ffcdd761938ad
DimonNikolaev <dmitriy.nikolaev1337@gmail.com>
2022-06-20 15:38:44 +0500

    Миграция для создания

---
+++ database/migrations/2022_06_20_092407_create_equipment_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('equipment', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('code');
+            $table->bigInteger('code_of_type_equipment');
+            $table->unique('equipment_serial_number_unique');
+            $table->text('note');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('equipment');
+    }
+};
