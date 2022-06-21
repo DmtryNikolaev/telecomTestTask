5f0fa2fa3924672ac2415bf4d4f71963d67ab921
DimonNikolaev <dmitriy.nikolaev1337@gmail.com>
2022-06-21 15:41:08 +0500

    Resource для /api/equipment

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\EquipmentController;
+use App\Models\Equipment;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resources([
+    '/api/equipment' => EquipmentController::class
+]);
