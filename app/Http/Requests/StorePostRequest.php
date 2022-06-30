540726dd8102127b6c0e892f4025c3e4bc00fa8e
DimonNikolaev <dmitriy.nikolaev1337@gmail.com>
2022-06-30 20:28:45 +0500

    Создание StorePostRequest для валидации

---
+++ app/Http/Requests/StorePostRequest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\EquipmentType;
+use Illuminate\Foundation\Http\FormRequest;
+
+require_once(resource_path('src/FormattedJsonString.php'));
+
+class StorePostRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        $serialNumberMask = EquipmentType::where('id', $this->input('code_of_type_equipment'))->first()->serial_number_mask;
+        $regularExpressions = [
+            'N' => '[0-9]',
+            'A' => '[A-Z]',
+            'a' => '[a-z]',
+            'X' => '[A-Z0-9]',
+            'Z' => '[-_@]'
+        ];
+        $snMaskSplit = mb_str_split($serialNumberMask);
+        $regex = collect($snMaskSplit)->map(function ($regex) use ($regularExpressions) {
+            return $regularExpressions[$regex];
+        })->implode('');
+
+        return [
+            'code_of_type_equipment' => 'required|min:0|max:100',
+            'serial_number' => ['required', function ($attribute, $snUnformat, $fail) use ($regex, $serialNumberMask) {
+                $serialNumbers = getFormattedJsonString($snUnformat)['sn'];
+
+                foreach ($serialNumbers as $serialNumber) {
+                    if (!preg_match_all("/^{$regex}/", $serialNumber)) {
+                        $fail("sn {$serialNumber}: {$serialNumberMask} не соответствует выбранному типу оборудования");
+                    }
+                }
+            }],
+            'note' => 'required|string'
+        ];
+    }
+}
