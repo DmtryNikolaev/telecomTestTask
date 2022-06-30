d742ff6042a8291d42e38b0abb2e99dd1142340b
DimonNikolaev <dmitriy.nikolaev1337@gmail.com>
2022-06-30 20:24:41 +0500

    Вынес валидацию в StorePostRequest

--- app/Http/Controllers/EquipmentController.php
+++ app/Http/Controllers/EquipmentController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StorePostRequest;
 use App\Models\Equipment;
 use App\Models\EquipmentType;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use function MongoDB\BSON\toJSON;
 
 class EquipmentController extends Controller
@@ -38,84 +40,13 @@ class EquipmentController extends Controller
      * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(StorePostRequest $request)
     {
         $serialNumberMask = EquipmentType::where('id', $request->input('code_of_type_equipment'))->first()->serial_number_mask;
-//        function isValidatedSerialNumber($snMask, $sn)
-//        {
-//            function getFormattedJsonString($value)
-//            {
-//                $valueReplaced = str_replace("'", '"', $value);
-//
-//                return json_decode($valueReplaced, true);
-//            }
-//            $regularExpressions = [
-//                'N' => '[0-9]',
-//                'A' => '[A-Z]',
-//                'a' => '[a-z]',
-//                'X' => '[A-Z0-9]',
-//                'Z' => '[-_@]'
-//            ];
-//            $serialNumberFormatted = getFormattedJsonString($sn);
-//            $serialNumbers = collect($sn)->keyBy('sn')->first();
-//            $snMaskSplit = mb_str_split($snMask);
-//            $regex = collect($snMaskSplit)->map(function ($regex) use ($regularExpressions) {
-//                return $regularExpressions[$regex];
-//            })->implode('');
-//
-//            foreach ($serialNumbers as $snElem) {
-//                if (!preg_match_all("/{$regex}/", $snElem)) {
-//                    return false;
-//                }
-//            }
-////            foreach ($serialNumbers as $snElem) {
-////                $serialNumbersSplit = mb_str_split($snElem);
-////
-////                foreach ($snMaskSplit as $elemOfSnMask) {
-////                    if (!preg_match("/{$regexInArr[$elemOfSnMask]}/", $snElem)) {
-////                        return false;
-////                    }
-////                }
-////            }
-//            return true;
-//        }
-        function getFormattedJsonString($value)
-        {
-            $valueReplaced = str_replace("'", '"', $value);
-            return json_decode($valueReplaced, true);
-        }
-//        $serialNumberFormatted = getFormattedJsonString($request->input('serial_number'));
-        $regularExpressions = [
-            'N' => '[0-9]',
-            'A' => '[A-Z]',
-            'a' => '[a-z]',
-            'X' => '[A-Z0-9]',
-            'Z' => '[-_@]'
-        ];
-//        $serialNumbers = collect($serialNumberFormatted)->keyBy('sn')->first();
-        $snMaskSplit = mb_str_split($serialNumberMask);
-        $regex = collect($snMaskSplit)->map(function ($regex) use ($regularExpressions) {
-            return $regularExpressions[$regex];
-        })->implode('');
+        $request->validated();
 
-        $data = $this->validate($request, [
-            'serial_number' => ['required', function ($attribute, $snUnformat, $fail) use ($regex, $serialNumberMask) {
-                $serialNumbers = getFormattedJsonString($snUnformat)['sn'];
-                foreach ($serialNumbers as $serialNumber) {
-                    if (!preg_match_all("/^{$regex}/", $serialNumber)) {
-                        $fail("sn {$serialNumber}: {$serialNumberMask} не соответствует выбранному типу оборудования");
-                    }
-                }
-            }]
-        ]);
-
-        $equipment = new Equipment();
-        $equipment->fill($data);
-        $equipment->save();
-
-        // Редирект на указанный маршрут
         return redirect()
-            ->route('articles.index');
+            ->route('equipment.index');
 
     }
 
