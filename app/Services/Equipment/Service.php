58401319d466e94cd4336defa22202b94af0bd8e
DimonNikolaev <dmitriy.nikolaev1337@gmail.com>
2022-07-07 01:10:02 +0500

    Рефакторинг Service.php

--- app/Services/Equipment/Service.php
+++ app/Services/Equipment/Service.php
@@ -5,23 +5,25 @@ namespace App\Services\Equipment;
 use App\Models\Equipment;
 use Illuminate\Database\Eloquent\Collection;
 use \Illuminate\Support\Collection as SupportCollection;
-use Illuminate\Http\Request;
+use Resources\EquipmentRequestHelper;
 
 class Service
 {
-    /**
-     * @param Request $request
-     * @return array
-     */
     public function getSerialNumbers(string $serialNumber): SupportCollection
     {
-        $serialNumbers = isset(getFormattedJsonString($serialNumber)['sn']) ?
-            getFormattedJsonString($serialNumber)['sn'] :
-            collect(getFormattedJsonString($serialNumber))->pluck('sn');
+        $formattedJson = (new EquipmentRequestHelper())->getFormattedJsonString($serialNumber);
+
+        $serialNumbers = isset($formattedJson['sn']) ?
+            $formattedJson['sn'] :
+            collect($formattedJson)->pluck('sn');
 
         return $serialNumbers;
     }
 
+    /**
+     * @param string $search
+     * @return Collection
+     */
     public function getSearchedData(string $search): Collection
     {
         $requiredData = Equipment::where('serial_number', 'LIKE', "%{$search}%")->orWhere('note', 'LIKE', "%{$search}%")->get();
